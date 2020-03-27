891dee92345ceba6a3125c5c378824a6b9a56696
Simar Kapoor <51281425+sikapoor@users.noreply.github.com>
2020-03-27 15:56:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,29 +17,10 @@ df = pd.read_csv('quotes.csv')
 
 app = Flask(__name__)
 
-"""
-data = {
-  "attachments": [],
-  "avatar_url": "http://i.groupme.com/123456789",
-  "created_at": 1302623328,
-  "group_id": "52652881",
-  "id": "52652881",
-  "name": "John",
-  "sender_id": "12345",
-  "sender_type": "user",
-  "source_guid": "GUID",
-  "system": false,
-  "text": "Hello world ☃☃",
-  "user_id": "1234567890"
-}
-"""
-
-
 
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
-  # We don't want to reply to ourselves!
   if datetime.datetime.today().weekday() == 0 or data['text'] == "@inspireme":
     r = randint(0,1664)
     auth = str(df.iloc[r][0])
@@ -47,11 +28,6 @@ def webhook():
     msg = quote + " - " + auth
     send_message(msg)
 
-    """
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    msg = "Forty hour workweeks are a relic of the Industrial Age. Knowledge workers function like athletes — train and sprint, then rest and reassess."
-    send_message(msg)
-    """
 
   return "ok", 200
 
@@ -71,15 +47,6 @@ def log(msg):
   print(str(msg))
   sys.stdout.flush()
 
-"""
-Registering my bot
-
-  {"meta":{"code":201},"response":{"bot":{"name":"Inspi_bot",
-  "bot_id":"50d85fe0f44b61086376584a48",
-  "group_id":"52652881","group_name":"Soon to be TS fans","avatar_url":null,
-  "callback_url":null,
-  "dm_notification":false}}}(base) Simars-MacBook-Pro:gmbot simar$ 
-"""
 
 
 
